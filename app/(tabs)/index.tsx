0be39bd4534c8a4f65201a6f60c737bddb9199ed
MarcosFilho1803 <132066643+MarcosFilho1803@users.noreply.github.com>
2024-05-13 21:59:45 -0300

    Melhorada a tela de listagem dos contatos

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,26 @@
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, TouchableHighlight} from 'react-native';
 import React from "react";
 import 'react-native-gesture-handler';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-function Home ({ navigation }){
+function Home ({navigation}){
   return(
-    <View style={styles.container}>
-      <View>
-      <Text style={styles.titulo}>Agenda de Contatos</Text>
+    <View style={styles.principal}>
+      <View style={styles.menu}>
+
+      <TouchableHighlight underlayColor={"white"} onPress={ () => navigation.navigate('AdcionarContato') } style = {styles.btnADD}>
+        <Text style={{fontSize: 30, fontWeight: '600'}}>+</Text>
+      </TouchableHighlight>
+
+      <TouchableHighlight underlayColor={"white"} onPress={ () => navigation.navigate('AdcionarContato') } style = {styles.btnADD}>
+        <Text style={{fontSize: 30, fontWeight: '600'}}>?</Text>
+      </TouchableHighlight>
       </View>
-      <Text>Hello World</Text>
-      <Button title="Adcionar Contato" onPress={ () => navigation.navigate('Adcionar Contato')} />
+        <View style={styles.titulo}>
+          <Text style={{fontSize: 30, fontWeight: '900', textAlign: 'center'}}>Contatos</Text>
+        </View>
     </View>
   )
 }
@@ -21,7 +29,7 @@ function Home ({ navigation }){
 function AdcionarContato ({ navigation }){
   return(
     <View style={styles.container}>
-      <Text>Contatos</Text>
+      <Text >Contatos</Text>
     </View>
   )
 }
@@ -30,7 +38,7 @@ export default function HomeScreen() {
   return (
      <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Adcionar Contato" component={AdcionarContato} />
+      <Stack.Screen name="AdcionarContato" component={AdcionarContato} />
     </Stack.Navigator>
 
 
@@ -39,17 +47,31 @@ export default function HomeScreen() {
 
 
 const styles = StyleSheet.create({
-  container: {
+  
+  principal: {
     flex: 1,
-    marginBottom: 500,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    elevation: 10,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      backgroundColor: 'white',
   },
-  titulo:{
-    marginBottom: 40,
-    fontSize: 20,
-    fontWeight: '900',
+  menu: {
+    position: 'absolute', 
+    top: 10, 
+    right: 10,
+    flexDirection: 'row'
+  },
+  btnADD:{
+    padding: 10
+  },
+  titulo: {
+    position: 'absolute', // Define a posição absoluta para o título
+    top: 60, // Posiciona o título no topo
+    left: 0, // Posiciona o título à esquerda
+    right: 0, // Posiciona o título à direita
+    height: 40, // Define a altura do título
+    justifyContent: 'center', // Centraliza verticalmente o texto do título
+    alignItems: 'center', // Centraliza horizontalmente o texto do título
+    backgroundColor: 'white', // Cor de fundo do título
+    
   }
 })
