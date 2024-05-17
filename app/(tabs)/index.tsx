252dfab64de8d313e7be933145dcf9798bff0e38
MarcosFilho1803 <132066643+MarcosFilho1803@users.noreply.github.com>
2024-05-16 22:09:43 -0300

    Interfaces

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import {View, Text, StyleSheet, Button, TouchableHighlight} from 'react-native';
+import {View, Text, StyleSheet, TextInput, TouchableHighlight, Button} from 'react-native';
 import React from "react";
 import 'react-native-gesture-handler';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -10,30 +10,25 @@ const Stack = createStackNavigator();
 function Home ({navigation}){
   return(
     <View style={styles.principal}>
+
       <View style={styles.menu}>
-
-      <TouchableHighlight underlayColor={"white"} onPress={ () => navigation.navigate('AdcionarContato') } style = {styles.btnADD}>
-        <Text style={{fontSize: 30, fontWeight: '600'}}>+</Text>
-      </TouchableHighlight>
-
-      <TouchableHighlight underlayColor={"white"} onPress={ () => navigation.navigate('AdcionarContato') } style = {styles.btnADD}>
-        <Text style={{fontSize: 30, fontWeight: '600'}}>?</Text>
-      </TouchableHighlight>
+        <TouchableHighlight underlayColor={"white"} onPress={ () => navigation.navigate('Adcionar Contato') } style = {styles.btnADD}>
+          <Text style={{fontSize: 30, fontWeight: '600'}}>+</Text>
+        </TouchableHighlight>
       </View>
+
         <View style={styles.titulo}>
           <Text style={{fontSize: 30, fontWeight: '900', textAlign: 'center'}}>Contatos</Text>
         </View>
 
         <View style={styles.contatoContainer}>
-          <View style={styles.cardContato}>
-            <Text>Junim Matador de Porco</Text>
-          </View>
-          <View style={styles.cardContato}>
-            <Text>Flavim do pneu</Text>
-          </View>
+        <TouchableHighlight underlayColor={"white"} onPress={ () => navigation.navigate('Editar') } style={styles.cardContato}>
+          <Text style={{ fontWeight: '400'}}>João clóvão</Text>
+        </TouchableHighlight>
+        <TouchableHighlight underlayColor={"white"} onPress={ () => navigation.navigate('Editar') } style={styles.cardContato}>
+          <Text style={{ fontWeight: '400'}}>Shaolin matador de porco</Text>
+        </TouchableHighlight>
         </View>
-        
-        
     </View>
   )
 }
@@ -41,8 +36,46 @@ function Home ({navigation}){
 
 function AdcionarContato ({ navigation }){
   return(
-    <View style={styles.container}>
-      <Text >Contatos</Text>
+    <View style={styles.principal}>
+     <View style={styles.titulo}>
+        <Text style={{fontSize: 30, fontWeight: '900', textAlign: 'center'}}>Novo Contato</Text>
+      </View>
+      <View style={styles.contatoContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder='Nome'
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Número'
+        />
+        <Button
+        title='Adcionar'
+        ></Button>
+      </View>
+    </View>
+  )
+}
+
+function EditarContato ({ navigation }){
+  return(
+    <View style={styles.principal}>
+     <View style={styles.titulo}>
+        <Text style={{fontSize: 30, fontWeight: '900', textAlign: 'center'}}>Editar Contato</Text>
+      </View>
+      <View style={styles.contatoContainer}>
+        <TextInput
+          style={styles.input}
+          value='João Clóvão'
+        />
+        <TextInput
+          style={styles.input}
+          value='996331803'
+        />
+        <Button
+        title='Editar'
+        ></Button>
+      </View>
     </View>
   )
 }
@@ -50,8 +83,9 @@ function AdcionarContato ({ navigation }){
 export default function HomeScreen() {
   return (
      <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="AdcionarContato" component={AdcionarContato} />
+      <Stack.Screen name="Contatos" component={Home} />
+      <Stack.Screen name="Adcionar Contato" component={AdcionarContato} />
+      <Stack.Screen name="Editar" component={EditarContato} />
     </Stack.Navigator>
 
 
@@ -68,32 +102,33 @@ const styles = StyleSheet.create({
       backgroundColor: 'white',
   },
   menu: {
-    position: 'absolute', 
-    top: 10, 
-    right: 10,
-    flexDirection: 'row'
+      flexDirection: 'row',
+      justifyContent: 'flex-end'
   },
   btnADD:{
-    padding: 10 
+    paddingRight: 30,
+    paddingTop: 20
   },
   titulo: {
-    position: 'absolute', // Define a posição absoluta para o título
-    top: 60, // Posiciona o título no topo
-    left: 0, // Posiciona o título à esquerda
-    right: 0, // Posiciona o título à direita
-    height: 40, // Define a altura do título
-    justifyContent: 'center', // Centraliza verticalmente o texto do título
-    alignItems: 'center', // Centraliza horizontalmente o texto do título
-    backgroundColor: 'white', // Cor de fundo do título
-    
+    position: 'relative', // Define a posição absoluta para o título
+    padding: 10
   },
   contatoContainer: {
-    backgroundColor: "#eee",
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 130
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'flex-start',
+      backgroundColor: "#eee",
+      padding: 20
+      
   },
   cardContato:{
     marginBottom: 10,
-  }
+   
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 })
